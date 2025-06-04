f8d23d44713277aebd12fbde89028a70b17b1673
Rakesh Potnuru <rakeshpotnuru321@gmail.com>
2025-06-04 11:39:48 +0530

    update post score update frequency

--- packages/core/src/feed/feed.controller.ts
+++ packages/core/src/feed/feed.controller.ts
@@ -7,7 +7,7 @@ import { FeedService } from "./feed.service";
 export class FeedController {
   constructor(private readonly feedService: FeedService) {}
 
-  @Cron(CronExpression.EVERY_2_HOURS)
+  @Cron(CronExpression.EVERY_WEEKEND)
   async updatePostsScore() {
     const posts = await this.feedService.getPosts();
 
